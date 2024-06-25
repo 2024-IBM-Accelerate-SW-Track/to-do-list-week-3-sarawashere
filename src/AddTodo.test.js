3ca078945e1966f8be9f434600c6ae12f00df08a
Sara Karnik <skarnik1@terpmail.umd.edu>
2024-06-24 22:29:54 -0400

    7th try

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -1,7 +1,7 @@
-import { render, screen} from '@testing-library/react';
+import { render, screen,fireEvent} from '@testing-library/react';
 import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
-import fireEvent from '@testing-library/react';
+import fireEvent from testing-library/react;
 import '@testing-library/jest-dom/extend-expect';
 
 let container = null;
