c2a02f4c72d510d3d4c7b4c0a6080bdb8493c271
Sara Karnik <skarnik1@terpmail.umd.edu>
2024-06-24 21:25:26 -0400

    7th try

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -1,6 +1,7 @@
 import { render, screen, fireEvent} from '@testing-library/react';
 import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
+import '@testing-library/jest-dom/extend-expect';
 
 let container = null;
 beforeEach(() => {
@@ -17,54 +18,75 @@ afterEach(() => {
 });
 
 
-
-
-
  test('test that App component doesn\'t render dupicate Task', () => {
   render(<App />);
- /*const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+ const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
   const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
   const element = screen.getByRole('button', {name: /Add/i});
   const dueDate = "05/30/2023";
-  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputTask, { target: { value: "In school Test"}});
   fireEvent.change(inputDate, { target: { value: dueDate}});
   fireEvent.click(element);
-  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputTask, { target: { value: "In school Test"}});
   fireEvent.change(inputDate, { target: { value: dueDate}});
   fireEvent.click(element);
-  const check=screen.getAllByText(/History Test/i);
-  expect(check.length).toBe(1);*/
+  const check=screen.getAllByText(new RegExp("In school Test","i"));
+  expect(check.length).toBe(1);
  });
 
  test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
-  /*const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
   const element = screen.getByRole('button', {name: /Add/i});
   const dueDate = "05/30/2023";
   fireEvent.change(inputDate, { target: { value: dueDate}});
   fireEvent.click(element);
   const check=screen.queryByText(new RegExp(dueDate,"i"));
-  expect(check.length).toBeNull();*/
+  expect(check.length).toBeNull();
  });
 
  test('test that App component doesn\'t add a task without due date', () => {
   render(<App />);
-  /*const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
   const element = screen.getByRole('button', {name: /Add/i});
-  const dueDate = "05/30/2023";
-  fireEvent.change(inputDate, { target: { value: dueDate}});
+  const name = "school test";
+  fireEvent.change(inputDate, { target: { value: name}});
   fireEvent.click(element);
-  const check=screen.queryByText(new RegExp(dueDate,"i"));
-  expect(check.length).toBeNull();*/
+  const check=screen.queryByText(new RegExp(name,"i"));
+  expect(check.length).toBeNull();
  });
 
 
 
  test('test that App component can be deleted thru checkbox', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+  fireEvent.change(inputTask, { target: { value: "In school Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+
+  const checkbox = screen.getByRole('checkbox');
+  fireEvent.click(checkbox);
+
+  const check=screen.queryByText(new RegExp("In school Test","i"));
+  expect(check.length).toBeNull();
  });
 
 
  test('test that App component renders different colors for past due events', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+  fireEvent.change(inputTask, { target: { value: "In school Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+
+  const card = screen.getByTestId(taskName);
+  expect(card).toHaveStyle('background-color: red');
+
  });
